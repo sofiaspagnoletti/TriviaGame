f0a95e6c96b80c777884ee6094168d5f3ca03333
sofi.spag <sofi.spag@gmail.com>
2019-06-06 20:22:13 -0700

    adding questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@ var myQuestions = [
     {
         question: "What is the name of Ross's son?",
         options: { a: "Oliver", b: "Noah", c: "Ben" },
-        correctanswer: options.c,
+        correctanswer: "c",
     },
     {
         question: "Which male friend has been married more than twice?",
@@ -16,52 +16,36 @@ var myQuestions = [
     },
     {
         question: "What was the name of the character Joey played on Days of Our Lives?",
-        options: {a: "Dr. Peter Tucker", b: "Dr. Drake Ramoray", c: "Dr. Samuel Heller"},
+        options: { a: "Dr. Peter Tucker", b: "Dr. Drake Ramoray", c: "Dr. Samuel Heller" },
         correctanswer: "b",
     },
     {
         question: "What was the name of the department store that Rachel worked for?",
-        options: {a: "Macy's", b:"Bloomingdale's", c:"Target"},
+        options: { a: "Macy's", b: "Bloomingdale's", c: "Target" },
         correctanswer: "b",
     },
     {
         question: "What is Monica's occupation?",
-        options: {a:"Waiter", b:"Chef", c: "Singer"},
+        options: { a: "Waiter", b: "Chef", c: "Singer" },
         correctanswer: "b",
     },
 ]
 
 var wins = 0;
 var losses = 0;
-var number = 40;
+var secondsInTimer = 10;
 var intervalId;
 
-function run() {
+function runTimer() {
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
 
 function decrement() {
 
-    //  Decrease number by one.
-    number--;
-
-    //  Show the number in the #show-number tag.
-    $("#time-left").html("<h2>" + number + "</h2>");
-
-
-    //  Once number hits zero...
-    if (number === 0) {
-
-        //  ...run the stop function.
-        stop();
-
-        //  Alert the user that time is up.
-        alert("Time Up!");
-    }
+    secondsInTimer--;
+    $("#time-left").html("<h2>" + secondsInTimer + "</h2>");
 }
-
-run();
 
 
 function win() {
@@ -74,18 +58,46 @@ function lose() {
     resetGame();
 }
 
+function stopGame() {
+    if (secondsInTimer === 0) {
+        $("#wins").text("Wins: " + wins);
+        $("#losses").text("Losses: " + losses);
+    }
+}
 
-$(document).ready(function () {
+userClickedOption =
 
-    $("#start").on("click", function () {
-        resetGame();
-        $("#goal").text("Goal: " + goal);
+    $(document).ready(function () {
+        $("#start").on("click", function () {
+            runTimer();
+            // $("#questions").text(" " + myQuestions.question);
+           
+            for (let i = 0; i < myQuestions.length; i++) {
+                const element = myQuestions[i];
+                var divQuestion = $("<div>");
+                divQuestion.text(" " + element.question);
+                $("#questions").append(divQuestion);
+                
+                
+                var divOptions = $("<div>");
+                // <input type="radio" name="gender" value="male"> Male<br></br>
+                var optionName = "answer" + i;
+                divOptions.html(
+                    '<input type="radio" name="' + optionName + '" value="a"> ' + element.options.a + ' <br/>' +
+                    '<input type="radio" name="' + optionName + '" value="b"> ' + element.options.b + ' <br/>' +
+                    '<input type="radio" name="' + optionName + '" value="c"> ' + element.options.c + ' <br/>');
+                // divOptions.text(" " + element.options.b);
+                // divOptions.text(" " + element.options.c);
+                $("#questions").append(divOptions);
+
+                // const content = $("#questions").html();
+                // $("#questions").html(content + element.question);
+                // $("#questions").html(" "+ element.options);
+            }
+           
+        });
+
+        stopGame();
     });
 
-    $("options").on("click", function (userChoice) {
-        for (let myQuestions = 0; myQuestions < array.length; myQuestions++) {
-            if (userChoice === correctanswer) { win(); }
-            else { lose() };
-        };
-    });
-});
+
